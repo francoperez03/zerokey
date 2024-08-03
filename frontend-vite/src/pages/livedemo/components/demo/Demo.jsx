5f5b0f08f04f548b473ecb2ec086554a295f5997
bmpaiba <brianmanuelpaiba@outlook.es>
2024-08-03 14:45:34 -0300

    checkout completo

--- frontend-vite/src/pages/livedemo/components/demo/Demo.jsx
+++ frontend-vite/src/pages/livedemo/components/demo/Demo.jsx
@@ -6,54 +6,56 @@ import Payment from "../../loader/Payment";
 import Completed from "../../loader/Completed";
 
 function Demo() {
+  const { status, setStatus, statuses } = useStore();
+  const [isActive, setIsActive] = useState(false); // Estado local para controlar el intervalo
+  const [intervalId, setIntervalId] = useState(null); // Estado para almacenar el ID del intervalo
 
-    const { status, setStatus, statuses } = useStore();
-    const [isActive, setIsActive] = useState(false); // Estado local para controlar el intervalo
-    const [intervalId, setIntervalId] = useState(null); // Estado para almacenar el ID del intervalo
+  console.log(status, "status");
 
-    console.log(status,"status")
-  
-    // Define el orden de los estados
-  
-    const startInterval = () => {
-      if (intervalId) return; // Si ya hay un intervalo en curso, no iniciar uno nuevo
-  
-      // Inicia el estado en 'generado' y luego cambia al siguiente estado
-      setStatus(statuses.PENDING);
-      setIsActive(true);
-  
-      // Cambia el estado a 'generando' después de hacer clic
-      setStatus(statuses.GENERATING);
-  
-      // Establece los intervalos para los cambios de estado
-      const id = setTimeout(() => {
-        setStatus(statuses.PAYING);
-  
-        const secondTimeout = setTimeout(() => {
-          setStatus(statuses.COMPLETED);
-          clearTimeout(secondTimeout); // Limpia el segundo timeout
-          setIsActive(false); // Detiene el ciclo
-        }, 3000); // Cambia a 'finalizado' después de 3 segundos
-      }, 3000); // Cambia a 'pagando' después de 3 segundos
-  
-      setIntervalId(id);
+  // Define el orden de los estados
+
+  const startInterval = () => {
+    if (intervalId) return; // Si ya hay un intervalo en curso, no iniciar uno nuevo
+
+    // Inicia el estado en 'generado' y luego cambia al siguiente estado
+    setStatus(statuses.PENDING);
+    setIsActive(true);
+
+    // Cambia el estado a 'generando' después de hacer clic
+    setStatus(statuses.GENERATING);
+
+    // Establece los intervalos para los cambios de estado
+    const id = setTimeout(() => {
+      setStatus(statuses.PAYING);
+
+      const secondTimeout = setTimeout(() => {
+        setStatus(statuses.COMPLETED);
+        clearTimeout(secondTimeout); // Limpia el segundo timeout
+        setIsActive(false); // Detiene el ciclo
+      }, 3000); // Cambia a 'finalizado' después de 3 segundos
+    }, 3000); // Cambia a 'pagando' después de 3 segundos
+
+    setIntervalId(id);
+  };
+
+  // Limpia el intervalo al desmontar el componente
+  useEffect(() => {
+    return () => {
+      if (intervalId) clearTimeout(intervalId);
     };
-  
-    // Limpia el intervalo al desmontar el componente
-    useEffect(() => {
-      return () => {
-        if (intervalId) clearTimeout(intervalId);
-      };
-    }, [intervalId]);
+  }, [intervalId]);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h5 className="text-lg pb-2">Enter fictitious card information</h5>
-      
-      {status === statuses.PENDING &&<Checkout startInterval={startInterval} /> }
+      {status === statuses.PENDING && (
+        <div>
+          <h5 className="text-lg pb-2 text-center">Enter fictitious card information</h5>
+          <Checkout startInterval={startInterval} />
+        </div>
+      )}
       {status === statuses.GENERATING && <GenerandoKey />}
-      {status === statuses.PAYING && <Payment/>}
-      {status === statuses.COMPLETED && <Completed/>}
+      {status === statuses.PAYING && <Payment />}
+      {status === statuses.COMPLETED && <Completed />}
     </div>
   );
 }
--- frontend-vite/src/pages/livedemo/loader/GenerandKey.jsx
+++ frontend-vite/src/pages/livedemo/loader/GenerandKey.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 function GenerandoKey() {
   return (
-    <div className='flex flex-col gap-4 p-4 justify-center items-center'>
+    <div className="flex flex-col gap-4 h-[450px] w-96 p-4 justify-center items-center bg-slate-50 rounde shadow-md shadow-gray-500">
+
         <img src="/assets/loading.gif" alt="" className='size-28' />
         <p className='text-xl'>Encriptando Informacion</p>
     </div>
