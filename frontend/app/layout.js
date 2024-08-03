2f1c518f787cea00ad40c3932564d49e67743868
bmpaiba <brianmanuelpaiba@outlook.es>
2024-08-02 22:36:33 -0300

    boton de prueba

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
       <ThemeProvider
             attribute="class"
-            defaultTheme="dark"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >{children}
---
+++ frontend/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
