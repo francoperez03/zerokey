889f3b18f19d8568747f26d6fd62eab8b91ed099
bmpaiba <brianmanuelpaiba@outlook.es>
2024-08-03 10:12:42 -0300

    base del checout en livedemo

--- frontend-vite/src/pages/livedemo/actions/formatCardNumber.js
+++
@@ -1,9 +0,0 @@
-export const  formatCardNumber = (event) => {
-    const input = event.target;
-    let value = input.value.replace(/\s+/g, ''); // Elimina espacios existentes
-    if (value.length > 16) {
-        value = value.slice(0, 16); // Limita a 16 dígitos
-    }
-    const formattedValue = value.replace(/(.{4})/g, '$1 ').trim(); // Formatea en grupos de 4 dígitos
-    input.value = formattedValue;
-}
