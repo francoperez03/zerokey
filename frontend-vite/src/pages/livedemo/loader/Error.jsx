14b3e4071be14f1fc55eb98092fd88e2f9cfb9a6
bmpaiba <brianmanuelpaiba@outlook.es>
2024-08-04 11:17:30 -0300

    error de checkout cuando no es el website

--- frontend-vite/src/pages/livedemo/loader/Error.jsx
+++ frontend-vite/src/pages/livedemo/loader/Error.jsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { VscError } from "react-icons/vsc";
 
 function Error() {
   return (
-    <div className="flex flex-col gap-4 h-[450px] w-96 p-4 justify-center items-center bg-slate-50 rounde shadow-md shadow-gray-500">
-      <VscError />
-      <p className="text-xl">Prueba inválida para este sitio</p>
+    <div className="flex flex-col gap-4 h-[450px] w-96 p-4 justify-around items-center bg-slate-50 rounde shadow-md shadow-gray-500">
+      <VscError className="text-9xl text-red-500"/>
+      <p className="text-2xl font-medium">Prueba inválida</p>
+      <a href="/#demo"  >
+        <Button className="rounded w-28 hover:text-red-500" variant="outline">
+            Back
+          </Button>
+        </a>
     </div>
   );
 }
--- frontend-vite/src/pages/livedemo/loader/PaymentCVV.jsx
+++ frontend-vite/src/pages/livedemo/loader/PaymentCVV.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function PaymentCVV() {
   return (
-    <div>PaymentCVV</div>
+    <div className="flex flex-col gap-4 h-[450px] w-96 p-4 justify-center items-center bg-slate-50 rounde shadow-md shadow-gray-500">
+
+      <div className="flex flex-col gap-4 p-4 justify-center items-center">
+        <img src="/assets/loading.gif" alt="" className="size-28" />
+        <p className="text-xl">Procesando pago</p>
+      </div>
+    </div>
   )
 }
 
